978745318cfbc89fd104900b0c1c26077b98e5b5
Terkulagodwin <terkulagodwin7@gmail.com>
2024-04-05 10:18:53 +0100

    my first React project

---
+++ flexbox/src/Flexbox.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Flexbox.css";
+
+function Flexbox() {
+  return (
+    <div className="Flex-container">
+      <div className="Flex-only">
+        <h1 className="lab1">Terkula</h1>
+        <h1 className="lab2">Sunday</h1>
+        <h1 className="lab3">Favour</h1>
+        <h1 className="lab4">Godoo</h1>
+      </div>
+
+      <div className="grid-container">
+        <h1 className="grid1">Ade</h1>
+        <h1 className="grid2">Winny</h1>
+        <h1 className="grid3">Ehud</h1>
+        <h1 className="grid4">Terk</h1>
+
+        <h1 className="grid5">Ade</h1>
+        <h1 className="grid6">Winny</h1>
+        <h1 className="grid7">Ehud</h1>
+        <h1 className="grid8">Terk</h1>
+
+        <h1 className="grid9">Ter</h1>
+        <h1 className="grid10">Winifred</h1>
+        <h1 className="grid11">Emma</h1>
+        <h1 className="grid12">Terkula</h1>
+
+        <h1 className="grid13">Ter</h1>
+        <h1 className="grid14">Winifred</h1>
+        <h1 className="grid15">Emma</h1>
+        <h1 className="grid16">Terkula</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Flexbox;
